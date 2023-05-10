17b23cc93021dd38468e1e9bff82a3bb743f9852
archisf <arkadyaristov@gmail.com>
2023-05-10 13:57:22 +0300

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,11 @@ import { filesystem, Entry } from './filesystem';
           [pageSize]="20"
           [sortable]="true"
           [sort]="sort"
-          
           [height]="800"
         >
-        <kendo-treelist-column [expandable]="true" field="name" title="Name" [width]="250">
+        <kendo-treelist-column [expandable]="true" field="name" title="Name" [width]="150">
             <ng-template kendoTreeListCellTemplate let-dataItem>
-                <span class="k-icon k-i-{{ dataItem.type === 'directory' ? 'folder' : 'file' }}"></span>
+                <span class="k-icon k-i-{{ dataItem.type === 'directory' ? 'tell-a-friend' : 'user' }}"></span>
                 {{ dataItem.name }}
             </ng-template>
         </kendo-treelist-column>
@@ -34,61 +33,12 @@ import { filesystem, Entry } from './filesystem';
             </kendo-treelist-column>
             <kendo-treelist-column field="size3" title="Size3" [width]="width">
             </kendo-treelist-column>
-
             <kendo-treelist-column field="size" title="Size" [width]="width">
             </kendo-treelist-column>
-            <kendo-treelist-column field="size2" title="Size2" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-
             <kendo-treelist-column field="size" title="Size" [width]="width">
             </kendo-treelist-column>
-            <kendo-treelist-column field="size2" title="Size2" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
 
-            <kendo-treelist-column field="size" title="Size" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size2" title="Size2" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
 
-            <kendo-treelist-column field="size" title="Size" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size2" title="Size2" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-
-            <kendo-treelist-column field="size" title="Size" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size2" title="Size2" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
-            <kendo-treelist-column field="size3" title="Size3" [width]="width">
-            </kendo-treelist-column>
 
         </kendo-treelist-column-group>
       </kendo-treelist>
@@ -96,7 +46,7 @@ import { filesystem, Entry } from './filesystem';
 })
 export class AppComponent {
   public data: Entry[] = filesystem;
-  width = 65
+  width = 100;
 
   public sort: SortDescriptor[] = [
     {
---
+++ src/app/utils.ts
@@ -0,0 +1,33 @@
+function dateRange(startDate, endDate, steps = 1): Date[] {
+  const dateArray = [];
+  const currentDate = new Date(startDate);
+
+  while (currentDate <= new Date(endDate)) {
+    dateArray.push(new Date(currentDate));
+    currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+  }
+  return dateArray;
+}
+
+const fieldNames = [
+  'workerName',
+  'zero',
+  'first',
+  'second',
+  'third',
+  'fourth',
+  'fifth',
+  'sixth',
+];
+
+const subFieldNames = {
+  0: 'zero.ShiftName',
+  1: 'first.ShiftName',
+  2: 'second.ShiftName',
+  3: 'third.ShiftName',
+  4: 'fourth.ShiftName',
+  5: 'fifth.ShiftName',
+  6: 'sixth.ShiftName',
+};
+
+export { dateRange, fieldNames, subFieldNames };
